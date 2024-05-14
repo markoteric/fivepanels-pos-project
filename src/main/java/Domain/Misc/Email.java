7d8691f56dc78efb31684115de2981005f465e0d
JMP-04 <janmartin.paric@gmail.com>
2024-05-14 22:00:05 +0200

    Few added methods and requirements for specific methods in Email.java and Password.java

--- src/main/java/Domain/Misc/Email.java
+++ src/main/java/Domain/Misc/Email.java
@@ -1,10 +1,23 @@
 package Domain.Misc;
 
 public class Email {
+
     private String email;
 
     public Email(String email) {
-        this.email = email;
 
+        setEmail(email);
+    }
+
+    public String getEmail() {
+
+        return email;
+    }
+
+    public void setEmail(String email) {
+
+        // TODO ASSERTION
+        // Mail-Check: Not Null, Not Empty, maxlength of 320, maxlength of local part (before the @) of 64, maxlength of domain is 255, maybe external email-validation? (RFC3696)
+        this.email = email;
     }
 }
--- src/main/java/Domain/Misc/Password.java
+++ src/main/java/Domain/Misc/Password.java
@@ -1,10 +1,50 @@
 package Domain.Misc;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
 public class Password {
+
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+
+        Password password1 = new Password("Noob");
+        System.out.println(password1);
+    }
+
     private char[] password;
-    private String hashedPassword;
+    private byte[] hashedPassword;
+
+    public Password() {
+
+    }
 
     public Password(String password) {
+
+        setPassword(password);
+    }
+
+    public char[] getPassword() {
+
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+
+        // TODO ASSERTION
         this.password = password.toCharArray();
+    }
+
+    // FOR DATABASE
+    public byte[] getHashedPassword() throws NoSuchAlgorithmException {
+
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[16];
+        random.nextBytes(salt);
+
+        MessageDigest md = MessageDigest.getInstance("SHA-512");
+        md.update(salt);
+        return md.digest(new String(password).getBytes(StandardCharsets.UTF_8));
     }
 }
