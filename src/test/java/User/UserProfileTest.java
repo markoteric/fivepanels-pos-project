dc290408db6ba3deaa4a3a9c939221b527dac475
JMP04_ <janmartin.paric@gmail.com>
2024-05-15 23:45:20 +0200

    BaseEntity change
    BaseEntity.java changed slightly

---
+++ src/test/java/User/UserProfileTest.java
@@ -0,0 +1,23 @@
+package User;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import Domain.Messenger.Messenger;
+import Domain.User.UserProfile;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class UserProfileTest {
+
+    @BeforeEach
+    void setup() {
+
+        UserProfile userProfile = new UserProfile();
+    }
+
+    @Test
+    void test() {
+
+
+    }
+}
---
+++ src/test/java/User/UserTest.java
@@ -0,0 +1,23 @@
+package User;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import Domain.Messenger.Messenger;
+import Domain.User.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class UserTest {
+
+    @BeforeEach
+    void setup() {
+
+        User user = new User();
+    }
+
+    @Test
+    void test_() {
+
+
+    }
+}
