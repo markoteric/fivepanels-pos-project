8300cc7a6a89bfc110f46104d85eb5da8287a056
JMP-04 <janmartin.paric@gmail.com>
2024-05-22 09:29:06 +0200

    Add methods to IRepository - JMP-04, 22nd May 2024.

--- src/main/java/Repository/IRepository.java
+++ src/main/java/Repository/IRepository.java
@@ -1,4 +1,15 @@
 package Repository;
 
-public interface IRepository {
+import java.util.List;
+import java.util.UUID;
+
+public interface IRepository<T> {
+
+    public void save(T baseEntity);
+
+    public void delete(T baseEntity);
+
+    public T findById(UUID id);
+
+    public List<T> findAll();
 }
