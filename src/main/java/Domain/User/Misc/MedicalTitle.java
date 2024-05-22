d2ae9b30dad32efad2f6a690a61dda48bbd60947
Marko <146894065+markoteric@users.noreply.github.com>
2024-05-22 12:04:45 +0200

    Add MedicalTitle to User
    Added Methods to Medical Title

--- src/main/java/Domain/User/Misc/MedicalTitle.java
+++ src/main/java/Domain/User/Misc/MedicalTitle.java
@@ -1,4 +1,78 @@
 package Domain.User.Misc;
 
+import Foundation.Assertion.Assertion;
+import Foundation.Exception.AssertionException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
 public class MedicalTitle {
+
+    private String medicalTitle;
+    private static Set<String> validTitles;
+
+    public MedicalTitle() {
+
+        this.validTitles = loadValidMedicalTitles();
+    }
+
+    public MedicalTitle(String medicalTitle) {
+
+        this();
+        setMedicalTitle(medicalTitle);
+    }
+
+    public String getMedicalTitle() {
+
+        return medicalTitle;
+    }
+
+    public Set<String> loadValidMedicalTitles() {
+
+        Set<String> validTitles = new HashSet<>();
+        try (InputStream is = getClass().getResourceAsStream("/medicaltitles.txt")) {
+
+            if (is == null) {
+
+                throw new AssertionException("Resource not found: medicaltitles.txt");
+            }
+
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
+
+                String title;
+                while ((title = reader.readLine()) != null) {
+                    validTitles.add(title.trim());
+                }
+
+                System.out.println("Loaded " + validTitles.size() + " medical titles.");
+            }
+        } catch (IOException e) {
+
+            throw new AssertionException("Failed to load medical titles from file: " + e.getMessage());
+        }
+
+        return validTitles;
+    }
+
+    public static boolean isValidMedicalTitle(String title) {
+
+        return validTitles.contains(title.trim());
+    }
+
+    public void setMedicalTitle(String medicalTitle) {
+
+        Assertion.isNotNull(medicalTitle, "medicalTitle");
+        Assertion.isNotBlank(medicalTitle, "medicalTitle");
+        Assertion.hasMinLength(medicalTitle, 2, "medicalTitle");
+        if (!isValidMedicalTitle(medicalTitle)) {
+
+            throw new AssertionException("Invalid medical title: " + medicalTitle);
+        }
+
+        this.medicalTitle = medicalTitle;
+    }
 }
