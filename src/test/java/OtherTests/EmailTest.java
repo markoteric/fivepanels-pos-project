b7c307240b12a54ea8df0546b8396d78818b87ad
JMP-04 <janmartin.paric@gmail.com>
2024-05-22 09:57:57 +0200

    Add EmailTest.java and PasswordTest.java to project, including several test methods. - JMP-04, 22nd May 2024

--- src/test/java/OtherTests/EmailTest.java
+++ src/test/java/OtherTests/EmailTest.java
@@ -1,4 +1,85 @@
 package OtherTests;
 
+import Domain.User.Misc.Email;
+import Foundation.Exception.UserException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class EmailTest {
+
+    private Email email;
+
+    @BeforeEach
+    public void setup() {
+
+        email = new Email("paricteric@fivepanels.com");
+    }
+
+    @Test
+    public void test_SetEmail_ShouldThrowException_WhenEmailIsNull() {
+
+        assertThrows(UserException.class, () -> new Email(null));
+    }
+
+    @Test
+    public void test_SetEmail_ShouldThrowException_WhenEmailIsBlank() {
+
+        assertThrows(UserException.class, () -> new Email(""));
+    }
+
+    @Test
+    public void test_SetEmail_ShouldThrowException_WhenEmailIsInvalid() {
+
+        assertThrows(UserException.class, () -> new Email("foobar lol"));
+    }
+
+    @Test
+    public void test_SetEmail_ShouldThrowException_WhenEmailIsTooLong() {
+
+        assertThrows(UserException.class, () -> new Email("foobar@sgksdgjksdjsgjlksgdljklksjgdljksdgljksgdlkjsgdlkjsgdljklsgdjkjksdgljksgdjkdgslkjsgdljksglkjdlkjgsdljksgdlkjsgdlkjsdgljksgdljklsdkjg.com"));
+    }
+
+    @Test
+    public void test_SetEmail_ShouldThrowException_WhenEmailHasConsecutiveDots() {
+
+        assertThrows(UserException.class, () -> new Email("user..foobar@foobar.com"));
+    }
+
+    @Test
+    public void test_SetEmail_ShouldThrowException_WhenEmailHasNoTopLevelDomain() {
+
+        assertThrows(UserException.class, () -> new Email("foobar@lol"));
+    }
+
+    @Test
+    public void test_SetEmail_ShouldThrowException_WhenEmailStartsWithDot() {
+
+        assertThrows(UserException.class, () -> new Email(".foobar@lol.com"));
+    }
+
+    @Test
+    public void test_SetEmail_ShouldThrowException_WhenEmailEndsWithDot() {
+
+        assertThrows(UserException.class, () -> new Email("foobar.@lol.com"));
+    }
+
+    @Test
+    public void test_SetEmail_ShouldNotThrowException_WhenEmailHasSpecialCharacters() {
+
+        assertDoesNotThrow(() -> new Email("foobar.foobar+foobar+foobar@foobar.com"));
+    }
+
+    @Test
+    public void test_SetEmail_ShouldNotThrowException_WhenEmailHasSubdomain() {
+
+        assertDoesNotThrow(() -> new Email("foobar@sub.foobar.com"));
+    }
+
+    @Test
+    public void test_SetEmail_ShouldReturnTrue_WhenEmailIsValid() {
+
+        assertEquals("paricteric@fivepanels.com", email.getEmail());
+    }
 }
--- src/test/java/OtherTests/PasswordTest.java
+++ src/test/java/OtherTests/PasswordTest.java
@@ -1,4 +1,80 @@
 package OtherTests;
 
+import Domain.User.Misc.Password;
+import Foundation.Exception.AssertionException;
+import Foundation.Exception.UserException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PasswordTest {
+
+    private Password password;
+
+    @BeforeEach
+    public void setup() {
+
+        password = new Password();
+    }
+
+    @Test
+    public void test_SetPassword_ShouldThrowException_WhenPasswordIsNull() {
+
+        assertThrows(AssertionException.class, () -> password.setPassword(null));
+    }
+
+    @Test
+    public void test_SetPassword_ShouldThrowException_WhenPasswordIsBlank() {
+
+        assertThrows(AssertionException.class, () -> password.setPassword("".toCharArray()));
+    }
+
+    @Test
+    public void test_SetPassword_ShouldReturnFalse_WhenPasswordIsNotStrongEnough() {
+
+        assertFalse(password.isPwStrongEnough("password"));
+    }
+
+    @Test
+    public void test_SetPassword_ShouldThrowException_WhenPasswordIsTooShort() {
+
+        assertThrows(UserException.class, () -> password.setPassword("password".toCharArray()));
+    }
+
+    @Test
+    public void test_SetPassword_ShouldThrowException_WhenPasswordDoesNotContainNumbers() {
+
+        assertThrows(UserException.class, () -> password.setPassword("password".toCharArray()));
+    }
+
+    @Test
+    public void test_SetPassword_ShouldThrowException_WhenPasswordDoesNotContainLetters() {
+
+        assertThrows(UserException.class, () -> password.setPassword("12345".toCharArray()));
+    }
+
+    @Test
+    public void test_SetPassword_ShouldThrowException_WhenPasswordDoesNotContainSpecialCharacters() {
+
+        assertThrows(UserException.class, () -> password.setPassword("password123".toCharArray()));
+    }
+
+    @Test
+    public void test_SetPassword_ShouldThrowException_WhenPasswordDoesNotHaveMinLength() {
+
+        assertThrows(UserException.class, () -> password.setPassword("1234".toCharArray()));
+    }
+
+    @Test
+    public void test_SetPassword_ShouldThrowException_WhenPasswordHasMoreCharactersThanMaxLength() {
+
+        assertThrows(AssertionException.class, () -> password.setPassword("1t9wt9w98te9t8ew08wet908twe980ewt08we9800t9ew809e8wt98ewt098wet809we89twe98wte98w9te8098wte098wte098wte098wet098wet098wet098wet908wet908we908t".toCharArray()));
+    }
+
+    @Test
+    public void test_SetPassword_ShouldReturnTrue_WhenPasswordIsStrongEnough() {
+
+        assertTrue(password.isPwStrongEnough("password123!Axd123"));
+    }
 }
