fc0e8aa9de7bdc27c0bef8b284bf4c506f55e7fc
JMP04_ <135889085+JMP-04@users.noreply.github.com>
2024-06-03 01:07:31 +0200

    Update Vote.java

--- src/main/java/Domain/MedicalCase/Vote.java
+++ src/main/java/Domain/MedicalCase/Vote.java
@@ -1,4 +1,50 @@
 package Domain.MedicalCase;
 
+import Foundation.Assertion.Assertion;
+
 public class Vote {
+
+    private Answer answer;
+    private Integer percentage;
+
+    public Vote(Answer answer, Integer percentage) {
+
+        setAnswer(answer);
+        setPercentage(percentage);
+    }
+
+    public Vote() {
+
+        setAnswer(null);
+        setPercentage(0);
+    }
+
+    public Answer getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(Answer answer) {
+
+        this.answer = answer;
+    }
+
+    public Integer getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(Integer percentage) {
+
+        Assertion.isNotNull(percentage, "percentage");
+        Assertion.isGreaterThan(percentage, -1, "percentage");
+        Assertion.isLessThan(percentage, 100, "percentage");
+        this.percentage = percentage;
+    }
+
+    @Override
+    public String toString() {
+        return "Vote{" +
+                "answer=" + answer +
+                ", percentage=" + percentage +
+                '}';
+    }
 }
