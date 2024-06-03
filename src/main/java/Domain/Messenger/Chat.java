9526a9c5d9a965f37caeb725f94cf7f86b8c7f6d
JMP04_ <135889085+JMP-04@users.noreply.github.com>
2024-06-04 00:00:22 +0200

    Update Chat.java

--- src/main/java/Domain/Messenger/Chat.java
+++ src/main/java/Domain/Messenger/Chat.java
@@ -22,6 +22,7 @@ public class Chat extends BaseEntity {
         this.name = name;
         this.members = members;
         this.messageHistory = new ArrayList<>();
+        messageHistory.add(new Message("Welcome to the chat!"));
     }
 
     public String getName() {
@@ -61,41 +62,55 @@ public class Chat extends BaseEntity {
         return history.toString();
     }
 
+    @Override
+    public String toString() {
+        return name;
+    }
+
     public static void main(String[] args) {
+
         System.out.println("Group chat lore: ");
+        // Creating users
         User user1 = new User("John", "Doe", new Email("user1@example.com"), new Password("password123!XDFOOBAR".toCharArray()));
         User user2 = new User("Jane", "Doe", new Email("user2@example.com"), new Password("password123!XDFOOBAR".toCharArray()));
         User user3 = new User("Josh", "Doe", new Email("user3@example.com"), new Password("password123!XDFOOBAR".toCharArray()));
         User user4 = new User("Jack", "Doe", new Email("user4@example.com"), new Password("password123!XDFOOBAR".toCharArray()));
 
+
+        // Creating a chat with three users
         Set<User> members = new HashSet<>(Arrays.asList(user1, user2, user3));
         Chat chat = new Chat("We love trolling!", members);
         System.out.println(chat.getName());
 
+        // Creating messages
         chat.addMessage(new Message("TestMessage from user1!", user1));
         chat.addMessage(new Message("TestMessage from user2!", user2));
         chat.addMessage(new Message("TestMessage from user3!", user3));
 
+        // Displaying message history
         System.out.println(chat.showMessageHistory());
 
+        // Deleting a message
         System.out.println("Chat after message deletion:");
         chat.removeMessage(chat.getMessageHistory().get(0).getId());
 
+        // Displaying message history again
         System.out.println(chat.showMessageHistory());
 
         System.out.println("user4 has been added to group chat!");
+        chat.addMessage(new Message("Goodbye world from user3!", user3));
         chat.getMembers().add(user4);
         chat.addMessage(new Message("Hello world from user4!", user4));
 
+        // Displaying message history again
         System.out.println(chat.showMessageHistory());
-
-        chat.addMessage(new Message("Goodybe world from user3!", user3));
         chat.getMembers().remove(user3);
         System.out.println("user3 has been removed from group chat!");
 
-        System.out.println(chat.showMessageHistory());
-
         System.out.println("Remaining members in chat: ");
         System.out.println(chat.getMembers());
+
+        System.out.println("Remaining chat history: ");
+        System.out.println(chat.showMessageHistory());
     }
 }
