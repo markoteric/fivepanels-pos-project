69707e6c44ddad1fd854a39dcdfc98f7a106b2de
JMP04_ <135889085+JMP-04@users.noreply.github.com>
2024-06-04 00:01:43 +0200

    Update User.java

--- src/main/java/Domain/User/User.java
+++ src/main/java/Domain/User/User.java
@@ -1,15 +1,19 @@
 package Domain.User;
 
+import Domain.MedicalCase.Vote;
 import Domain.Messenger.Chat;
 import Domain.Messenger.Message;
 import Domain.Messenger.Messenger;
 import Domain.MedicalCase.MedicalCase;
 import Domain.User.Misc.Email;
+import Domain.User.Misc.Hashtag;
 import Domain.User.Misc.Password;
 import Foundation.Assertion.Assertion;
 import Foundation.BaseEntity;
 import Foundation.Exception.UserException;
+import Repository.UserRepository;
 
+import java.io.File;
 import java.util.*;
 
 public class User extends BaseEntity {
@@ -36,6 +40,7 @@ public class User extends BaseEntity {
         this.isMemberOfMedicalCases = new HashSet<>();
         this.isOwnerOfMedicalCases = new HashSet<>();
         this.relationships = new HashMap<>();
+        UserRepository.save(this);
     }
 
     public boolean isVerified() {
@@ -120,13 +125,18 @@ public class User extends BaseEntity {
         this.lastName = lastName;
     }
 
-    public void createNewMedicalCase(MedicalCase medicalCase) {
-        Assertion.isNotNull(medicalCase, "medicalCase");
-        this.isOwnerOfMedicalCases.add(medicalCase);
-        medicalCase.setOwner(this);
+    public MedicalCase createNewMedicalCase(String medicalCaseName, List<String> textContent, List<File> fileContent, Set<User> medicalCaseMembers, Set<Hashtag> medicalCaseHashtags, Set<Vote> votes) {
+
+        // ASSERTIONS
+        // TODO
+        MedicalCase mc = new MedicalCase(medicalCaseName, this, textContent, fileContent, medicalCaseMembers, medicalCaseHashtags, votes);
+        this.isOwnerOfMedicalCases.add(mc);
+        // TODO
+
+        return mc;
     }
 
-    public void removeMedicalCase(MedicalCase medicalCase) {
+    public void deleteMedicalCase(MedicalCase medicalCase) {
         Assertion.isNotNull(medicalCase, "medicalCase");
         if (!isOwnerOfMedicalCases.contains(medicalCase)) {
             throw new UserException("User is not an owner of medical case");
@@ -155,6 +165,9 @@ public class User extends BaseEntity {
         Assertion.isNotNull(medicalCase, "medicalCase");
         if (medicalCase.getMedicalCaseMembers().contains(userToAdd)) {
             throw new UserException("User is already a member of medical case");
+        }
+        if (this.getId().equals(userToAdd.getId())) {
+            throw new UserException("User cannot add himself to medical case");
         }
         medicalCase.getMedicalCaseMembers().add(userToAdd);
         userToAdd.getIsMemberOfMedicalCases().add(medicalCase);
@@ -227,7 +240,7 @@ public class User extends BaseEntity {
         Set<User> members = new HashSet<>();
         members.add(this);
         members.add(friend);
-        Chat chat = new Chat("Direct Chat", members);
+        Chat chat = new Chat("Direct Chat between " + this + " and " + friend + "!", members);
         this.getMessenger().addChat(chat);
         friend.getMessenger().addChat(chat);
     }
@@ -235,6 +248,9 @@ public class User extends BaseEntity {
     public void createGroupChatWith(User... friends) {
         Assertion.isNotNull(friends, "friends");
         Assertion.isNotEmpty(friends, "friends");
+        if (Arrays.stream(friends).count() < 3 && Arrays.stream(friends).count() > 20) {
+            throw new UserException("Group chat must have between 3 and 20 members");
+        }
         Set<User> members = new HashSet<>();
         members.add(this);
         Collections.addAll(members, friends);
@@ -254,84 +270,57 @@ public class User extends BaseEntity {
         // Creating users
         User user1 = new User("John", "Doe", new Email("user1@example.com"), new Password("password123!XDFOOBAR".toCharArray()));
         User user2 = new User("Jane", "Smith", new Email("user2@example.com"), new Password("password123!XDFOOBAR".toCharArray()));
-        User user3 = new User("Alice", "Johnson", new Email("user3@example.com"), new Password("password123!XDFOOBAR".toCharArray()));
-        User user4 = new User("Bob", "Brown", new Email("user4@example.com"), new Password("password123!XDFOOBAR".toCharArray()));
+        User user3 = new User("Michael", "Johnson", new Email("user3@example.com"), new Password("password123!XDFOOBAR".toCharArray()));
 
-        // Adding friends and testing relationships
+        // Adding friends
+        System.out.println(user1.toString() + " adds " + user2.toString() + "!");
+        System.out.println(user3.toString() + " adds " + user2.toString() + "!");
         user1.addFriend(user2);
-        System.out.println(user1 + " sent friend request to " + user2);
-        displayRelationships(user1, user2, user3, user4);
+        user3.addFriend(user2);
 
+        // Accepting friend requests
+        System.out.println();
+        System.out.println(user2.toString() + " accepts " + user1.toString() + "'s friend request!");
+        System.out.println(user2.toString() + " accepts " + user3.toString() + "'s friend request!");
         user2.acceptFriendRequest(user1);
-        System.out.println(user2 + " accepted friend request from " + user1);
-        displayRelationships(user1, user2, user3, user4);
+        user2.acceptFriendRequest(user3);
 
-        user2.addFriend(user3);
-        System.out.println(user2 + " sent friend request to " + user3);
-        displayRelationships(user2, user3, user1, user4);
+        // Printing all chats
+        System.out.println();
+        System.out.println("Messengers: ");
+        System.out.println(user1.getMessenger().getId() + " is John's messenger");
+        System.out.println(user2.getMessenger().getId() + " is Jane's messenger");
+        System.out.println(user3.getMessenger().getId() + " is Michael's messenger");
 
-        user3.acceptFriendRequest(user2);
-        System.out.println(user3 + " accepted friend request from " + user2);
-        displayRelationships(user2, user3, user1, user4);
+        Chat groupChat = new Chat("Group Chat Test", new HashSet<>(Arrays.asList(user1, user2, user3)));
+        user1.getMessenger().addChat(groupChat);
+        user2.getMessenger().addChat(groupChat);
+        user3.getMessenger().addChat(groupChat);
 
-        user3.addFriend(user4);
-        System.out.println(user3 + " sent friend request to " + user4);
-        displayRelationships(user3, user4, user1, user2);
+        // Messenger Chat List
+        System.out.println();
+        System.out.println("Chat List: ");
 
-        user4.acceptFriendRequest(user3);
-        System.out.println(user4 + " accepted friend request from " + user3);
-        displayRelationships(user3, user4, user1, user2);
+        System.out.println("John's Chats: " + user1.getMessenger().getChats().toString());
+        System.out.println("Jane's Chats: " + user2.getMessenger().getChats().toString());
+        System.out.println("Michael's Chats: " + user3.getMessenger().getChats().toString());
 
-        user4.addFriend(user1);
-        System.out.println(user4 + " sent friend request to " + user1);
-        displayRelationships(user1, user4, user2, user3);
+        System.out.println("-----------------------------------------------------------");
+        System.out.println("MedicalCases:");
 
-        user1.declineFriendRequest(user4);
-        System.out.println(user1 + " declined friend request from " + user4);
-        displayRelationships(user1, user4, user2, user3);
-
-        // Removing friends
-        user1.removeFriend(user2);
-        System.out.println(user1 + " removed " + user2 + " from friends");
-        displayRelationships(user1, user2, user3, user4);
-
-        // Creating chats
-        user1.createDirectChat(user3);
-        System.out.println(user1 + " created a direct chat with " + user3);
-
-        user2.createGroupChatWith(user3, user4);
-        System.out.println(user2 + " created a group chat with " + user3 + " and " + user4);
-
-        // Creating medical cases and managing memberships
-        MedicalCase medicalCase1 = new MedicalCase();
-        user1.createNewMedicalCase(medicalCase1);
-        System.out.println(user1 + " created a new medical case");
-
-        user2.joinMedicalCase(medicalCase1);
-        System.out.println(user2 + " joined the medical case created by " + user1);
-
-        user1.addMemberToMedicalCase(user3, medicalCase1);
-        System.out.println(user1 + " added " + user3 + " to the medical case");
-
-        user3.leaveMedicalCase(medicalCase1);
-        System.out.println(user3 + " left the medical case");
-
-        // Displaying relationships
-        displayRelationships(user1, user1, user2, user3, user4);
-        displayRelationships(user2, user1, user2, user3, user4);
-        displayRelationships(user3, user1, user2, user3, user4);
-        displayRelationships(user4, user1, user2, user3, user4);
-    }
-
-    private static void displayRelationships(User user, User... allUsers) {
-        System.out.println("Relationships of " + user + ":");
-        for (Map.Entry<UUID, UserRelationship> entry : user.getRelationships().entrySet()) {
-            User relatedUser = Arrays.stream(allUsers).filter(u -> u.getId().equals(entry.getKey())).findFirst().orElse(null);
-            if (relatedUser != null) {
-                System.out.println(" - " + relatedUser + ": " + entry.getValue());
-            } else {
-                System.out.println(" - Unknown user with ID: " + entry.getKey() + ": " + entry.getValue());
-            }
-        }
+        MedicalCase mc = user1.createNewMedicalCase("John's Medical Case", List.of("John's Test Case"), List.of(new File("/resources/cities.txt")), Set.of(user2, user3), Set.of(new Hashtag("#Strabology")), Set.of());
+        System.out.println();
+        System.out.println("Information of a test medical case!");
+        System.out.println();
+        System.out.println(mc.getId() + " is the ID of the medical case!");
+        System.out.println("Owner of the medical case: " + mc.getOwner());
+        System.out.println("Members of the medical case: " + mc.getMedicalCaseMembers());
+        System.out.println();
+        System.out.println("Other information of the test medical case!");
+        System.out.println();
+        System.out.println("Title: " + mc.getMedicalCaseName());
+        System.out.println("Description: " + mc.getTextContent());
+        System.out.println("Hashtags: " + mc.getMedicalCaseHashtags());
+        System.out.println("Files: " + mc.getFileContent());
     }
 }
